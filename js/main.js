337c3bcb938043877780759ac161ed0e8fc05611
stieludv <19951864+stieludv@users.noreply.github.com>
2023-05-05 23:02:24 +0200

    Adding books dynamically to HTML using JS

--- js/main.js
+++ js/main.js
@@ -27,9 +27,29 @@ books.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, 78));
 books.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, 295));
 console.log(books)
 
+// Create the book elements for the book-container
+function createBookElement(book) {
+    const bookElement = document.createElement("div");
+    const closeButtonElement = document.createTextNode("x");
+    const bookInfoElement = document.createTextNode(book.info());
+    bookElement.append(closeButtonElement, bookInfoElement)
+
+    // I also need an input at the bottom to adjust the current page I'm on
+    // I also need a bookmark icon to click to open the modal for bookmarks (digital bookmarks of pages + notes)
+    // Book front cover image?
+    return bookElement;
+}
+
 // Get the container element where we want to add books
 const bookContainer = document.querySelector(".book-container");
 
+// Get the "book" that we want to insert before
+const addBook = document.querySelector(".add-book");
 
+// For every book in books, generate and insert book element
+books.forEach(book => {
+    const bookElement = createBookElement(book);
+    bookContainer.insertBefore(bookElement, addBook);
+});
 
 console.log(bookContainer);
